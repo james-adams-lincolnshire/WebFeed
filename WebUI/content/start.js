4cff124d4e357b5d4c1c6c523d5e8326406fcb15
James <james.adams@outlook.com>
2015-08-16 05:14:55 +0100

    Added functionality to load javascript dependancies, from javascript.

--- WebUI/content/start.js
+++ WebUI/content/start.js
@@ -1,8 +1,55 @@
-﻿var app = {};
+﻿var app = {
+	dependancies: {
+		src: [ // enter javascript source URL's below.
+		],
+		callback,
+		loadedCount: 0,
+		loading: false,
+		load: function (srcUrls, callback) {
+			app.dependancies.callback = callback;
 
-app.start = function()
-{
-	alert('hellow world');
-}
+			for (var i = 0; i < srcUrls.length; i++) {
+				var script = document.createElement('script');
 
-app.start();
+				script.type = 'text/javascript';
+				script.src = srcUrls[i];
+				
+				// Two callback types, for cross browser support.
+				script.onreadystatechange = loadedCallback;
+				script.onload = loadedCallback;
+
+				document.getElementsByTagName('head')[i].appendChild(script);
+			}
+
+			app.dependancies.loading = true;
+		},
+		loaded: function () {
+			var loaded = !app.dependancies.src ||
+						 app.dependancies.src.length === 0 ||
+						 app.dependancies.loadedCount === app.dependancies.src.length;
+
+			if (!loaded) {
+				app.dependancies.loadedCount ? app.dependancies.loadedCount++ : app.dependancies.loadedCount = 1;
+				loaded = app.dependancies.loadedCount == app.dependancies.src.length;
+			}
+			else
+				app.dependancies.loading = false;
+
+			return loaded;
+		},
+		loadedCallback: function() {
+			if (app.dependancies.loaded())
+				app.dependancies.callback();
+		}
+	}
+};
+
+app.main = function () {
+	app.dependancies.load(app.dependancies.src, function () {
+		// do something!?
+	});
+};
+
+app.loadFeed = function () {
+	var url = 'http://feeds.bbci.co.uk/sport/0/football/rss.xml?edition=uk';
+};
